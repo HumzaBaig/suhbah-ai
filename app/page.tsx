25a1180d8e4a315a0d0e2089c381cdaf93afb57c
HumzaBaig <humzacodemail@gmail.com>
2023-08-11 15:04:54 -0500

    edit app/page.tsx file to check if edits work in real-time

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div className="text-3xl text-color-green"></div>;
+  return <div className="text-3xl text-green-500">Hello Suhbah AI</div>;
 }
